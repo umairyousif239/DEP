6c38066022ab8f7d0e6d4c7d645b6b75228f1a1e
Umair Yousif <37881515+umairyousif239@users.noreply.github.com>
2024-08-31 13:35:51 +0500

    minor alterations

--- 4. RESTfulAPI/config.py
+++ 4. RESTfulAPI/config.py
@@ -3,7 +3,7 @@ import os
 
 # Define the application directory
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'b4b6d862cc8bb31049f4d1a75b530db07dc89d6a238464475f06f7c1b1842cf0')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'b4b6d862cc8bb31049f4d1a75b530db07dc89d6a238464475f06f7c1b1842cf0') # Secret key generated with secrets module
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'e9fe1d06fd44dc147ddb32f7997eeecdbc7d4412a8356f11408b5fc5347eca31'  # Update this with a secure key
+    JWT_SECRET_KEY = 'e9fe1d06fd44dc147ddb32f7997eeecdbc7d4412a8356f11408b5fc5347eca31'  # Secret key generated with secrets module
